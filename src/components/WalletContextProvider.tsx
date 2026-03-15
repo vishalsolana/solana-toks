b54271b121a909cd24f07c91d21bd13bf08f45cf
Vis <imcryptovishal@gmail.com>
2026-03-15 12:50:26 +0530

    fix: remove BackpackWalletAdapter

--- src/components/WalletContextProvider.tsx
+++ src/components/WalletContextProvider.tsx
@@ -8,12 +8,8 @@ import {
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import { PhantomWalletAdapter } from "@solana/wallet-adapter-phantom";
-import {
-  SolflareWalletAdapter,
-  BackpackWalletAdapter,
-} from "@solana/wallet-adapter-wallets";
+import { SolflareWalletAdapter } from "@solana/wallet-adapter-wallets";
 
-// Required CSS for the wallet modal
 import "@solana/wallet-adapter-react-ui/styles.css";
 
 interface WalletContextProviderProps {
@@ -29,11 +25,7 @@ export const WalletContextProvider: FC<WalletContextProviderProps> = ({
     "https://rpc.solanatracker.io/public";
 
   const wallets = useMemo(
-    () => [
-      new PhantomWalletAdapter(),
-      new SolflareWalletAdapter(),
-      new BackpackWalletAdapter(),
-    ],
+    () => [new PhantomWalletAdapter(), new SolflareWalletAdapter()],
     []
   );
 
